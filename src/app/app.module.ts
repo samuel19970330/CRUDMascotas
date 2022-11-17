6de8cc642d11722ab65fadf18da2453a1fb5bdb2
sistemas5 <sistemas5@ipuc.org.co>
2022-11-17 17:00:45 -0500

    Cambios Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+//Componentes 
+import { AgregarEditarMascotaComponent } from './components/agregar-editar-mascota/agregar-editar-mascota.component';
+import { ListadoMascotaComponent } from './components/listado-mascota/listado-mascota.component';
+import { VerMascotaComponent } from './components/ver-mascota/ver-mascota.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+//Angular Material
+import {MatTableModule} from '@angular/material/table';
+import {MatPaginatorModule} from '@angular/material/paginator';
+import {MatSortModule} from '@angular/material/sort';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgregarEditarMascotaComponent,
+    ListadoMascotaComponent,
+    VerMascotaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/listado-mascota/listado-mascota.component.ts
@@ -0,0 +1,49 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Mascota } from 'src/app/interfaces/mascota';
+
+
+
+const listMascotas: Mascota[] = [
+{ nombre: 'Nala', edad: 1, raza: 'Pastor Alemán', color: 'Café', peso: 25},
+{ nombre: 'Lupe', edad: 14, raza: 'Schnauzer', color: 'Gris', peso: 7},
+{ nombre: 'Dana', edad: 6, raza: 'Maine Coon', color: 'Pardo', peso: 5},
+{ nombre: 'Rose', edad: 2, raza: 'Maine Coon', color: 'Blanco', peso: 3},
+{ nombre: 'Roger', edad: 11, raza: 'Criollo', color: 'Gris', peso: 8},
+{ nombre: 'Mona', edad: 12, raza: 'Schnauzer', color: 'Café', peso: 10}
+
+];
+
+@Component({
+  selector: 'app-listado-mascota',
+  templateUrl: './listado-mascota.component.html',
+  styleUrls: ['./listado-mascota.component.css']
+})
+export class ListadoMascotaComponent implements OnInit, AfterViewInit {
+
+  displayedColumns: string[] = ['nombre','edad','raza','color','peso'];
+  dataSource = new MatTableDataSource<Mascota>(listMascotas);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+    this.paginator._intl.itemsPerPageLabel = 'Items por Página'
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+}
